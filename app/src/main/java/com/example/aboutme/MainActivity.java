d5d58b834ae9eade7e7d940c0453b4f297d5a9a9
binay shah <binayshah@binays-MacBook-Pro.local>
2021-02-10 08:40:17 +0545

    initial commit

---
+++ app/src/main/java/com/example/aboutme/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.aboutme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    private void addNickName(View v){
+        EditText nickNameEdit = findViewById(R.id.nickname_edit);
+        String nickNameText = nickNameEdit.getText().toString();
+        TextView nickNameTextView = findViewById(R.id.nickname_text);
+        nickNameTextView.setText(nickNameText);
+        nickNameTextView.setVisibility(View.VISIBLE);
+
+        Context context = v.getContext();
+
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+    }
+}
